5108c67f2dfd199651380402a4ffcfc2e3011243
H4lu <halurecht@gmail.com>
2018-06-11 14:35:38 +0300

    working web3 and btc lib

--- src/crypto/Ethereum.ts
+++ src/crypto/Ethereum.ts
@@ -2,6 +2,7 @@ import * as rpc from 'ethrpc'
 declare global {
     interface Window {
         ethrpc: any
+        Web3: any
     }
 }
 let connectionConfiguration = {
@@ -14,20 +15,9 @@ let connectionConfiguration = {
   };
 
 export function getETBALANCE() {
-    window.ethrpc.connect(connectionConfiguration, (err) => {
-        if (err) {
-            console.error('Failed to connect to Ethereum node')
-        } else {
-            console.log('Connected to Eth node!')
-            window.ethrpc.eth.getBalance("0x619B30BE614ce453035058736cd2B83c34373Ddd",function(err,res) {
-                console.log('error',err)
-                console.log(res)
-            },1)
-        }
-    })
-    /* let web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/hgAaKEDG9sIpNHqt8UYM'))
+    let web3 = new window.Web3(new window.Web3.providers.HttpProvider('https://ropsten.infura.io/hgAaKEDG9sIpNHqt8UYM'))
     console.log('web3 object', web3)
    
-    web3.eth.getBalance('0x619B30BE614ce453035058736cd2B83c34373Ddd')
-    */
+    console.log(web3.eth.getBalance('0x619B30BE614ce453035058736cd2B83c34373Ddd'))
+
 }
